97501de725b3b503788391877a628057a2664f9b
Zach Walsh <45351218+DaTimeKeeper@users.noreply.github.com>
2021-12-03 12:12:47 -0700

    Update ss.py

--- ss.py
+++ ss.py
@@ -8,9 +8,5 @@
 # Member2 Name: XXXXXX
 # Member2 SIS ID: XXXXXX
 # Member2 Login ID: XXXXXX
-
-# Member3 Name: Zach Walsh
-# Member3 SIS ID: 831955300
-# Member3 Login ID: zachwals
 ###############################################
 
