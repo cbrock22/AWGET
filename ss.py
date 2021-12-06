25dd0fe0a97563d64bc9b92dcb509a5ba6f06fa8
Cole <cole.brock@gmail.com>
2021-12-05 22:17:01 -0700

    major upgrade: threaded tcp handler

--- ss.py
+++ ss.py
@@ -13,50 +13,99 @@
 # Member3 SIS ID: 831955300
 # Member3 Login ID: zachwals
 ###############################################
-
+from __future__ import unicode_literals
 import sys
+import os
+import random
 import socket
 from socket import AF_INET, SOCK_STREAM
+from threading import Thread
+from socketserver import ThreadingMixIn
 
-# def threaded(c):
-#     while c:
-#         data = c.recv(1024)
-#         if not data:
-#             print('No message close connection')
-#             print_lock.release()
+class ClientThread(Thread):
 
-def stepper(portNum):
-    print('starting stepper...')
-    conn = socket.gethostname()
-    print(conn)
-    IP = socket.gethostbyname(conn)
-    print(IP)
-    soc = socket.socket(AF_INET, SOCK_STREAM)
-    soc.bind(('127.0.0.1', int(portNum)))
-    soc.listen(5)
-    print('socket with IP {} on port {} is listening...'.format(IP,portNum))
-    threads = []
+    def __init__(self, ip, port):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        print("[+] a new thread has started on {}:{}".format(ip,port))
 
-    #while True:
-    c, addr = soc.accept()
-    data = c.recv(1024)
-    print('data recieved from {} :'.format(addr))
-    print('')
-    print(data)
-    print('')
-    print('exiting program')
-    c.close()
-    soc.close()
+    def run(self):
+        while True:
+            try:
+                data = c.recv(262144)
+                data = (data.decode("ascii"))
+                print(data)
+                x = data.split('\n')
+                url = x.pop(0)
+                y = url.split('/')
+                filename = y.pop()
+                print(filename)
+                #awgetresponse = "response\n"
+                if len(x) == 0:
+                    print("the list is empty!")
+                    os.system("wget {}".format(url))
+                    f = open('./index.html', 'r')
+                    awgetresponse = f.readlines()
+                    print(len(awgetresponse))
+                    for element in awgetresponse:
+                        element += '\n'
+                    #print(len(awgetresponse))
+                    #print(awgetresponse[0])
+                    #print("-------AWGET RESPONSE---------")
+                    awgetresponse  = str(awgetresponse)
+                    print(len(awgetresponse))
+
+                    
+                    c.send(awgetresponse.encode("ascii"))
+                        
+                if len(x) != 0:
+                    print("the list is not empty!")
+                    randindex = random.randint(0, len(x)-1)
+                    nextss = x.pop(randindex)
+                    nextss.split(' ')
+                    soc2 = socket.socket(AF_INET, SOCK_STREAM)
+                    soc2.connect(nextss[0], nextss[1])
+                    print("ss has connected to nextss with IP: {} and port: {}".format(nextss[0], nextss[1]))
+                    message = url
+                    message += str(x)
+                    soc2.send(message.encode("ascii"))
+                    data2 = soc2.recv(4096)
+                    c.send(data2)
+                    soc2.close()
+                    c.close()
+                break
+            except KeyboardInterrupt:
+                break
 
 
+if(len(sys.argv) != 2):
+    print('Please specify only one port number.')
+    exit(1)
+portNum = sys.argv[1]
+print('starting stepper...')
+conn = socket.gethostname()
+print(conn)
+IP = socket.gethostbyname(conn)
+print(IP)
+soc = socket.socket(AF_INET, SOCK_STREAM)
+soc.bind(('127.0.0.1', int(portNum)))
+soc.listen(5)
+#print('socket with IP {} on port {} is listening...'.format(IP,portNum))
+threads = []
 
+while True:
+    try:
+        (c, (sourceip,sourceport)) = soc.accept()
+        newthread = ClientThread(sourceip,portNum)
+        newthread.start()
+        threads.append(newthread)
+        break
+    except KeyboardInterrupt:
+        c.close()
+        soc.close()
+        break
 
-def main():
-    if(len(sys.argv) != 2):
-        print('Please specify only one port number.')
-        exit(1)
-    stepper(sys.argv[1])
+for t in threads:
+    t.join()
 
-
-if __name__ == "__main__":
-    main()
--- testclient.py
+++ testclient.py
@@ -1,33 +1,88 @@
 # Import socket module
 import socket
-  
-  
-def Main():
+import sys
+import random
+
+def Main(argv):
+
+    # read file
+    url=""
+    filename="chaingang.txt"
+    ssInfo=[]
+    totalSS=0
+    if len(argv)==1:
+        url=argv[0]
+    elif len(argv)==2:
+        url=argv[0]
+        filename=argv[1]
+    else:
+        print("error")
+        return 1
+    
+    try:    
+        f=open(filename)
+        data=f.readlines()
+        
+        f.close()
+        totalSS=int(data[0][0])
+        for x in range(int(totalSS)):
+            if(x+1<=totalSS):
+                tIp,tPort=data[x+1].split()
+                ssInfo.append((tIp,int(tPort)))
+            
+    except:
+        print("error")
+        return 1
+        
+    # print(url)
+    #print(ssInfo)
+    ssIndex=random.randint(0,totalSS-1)
+    #ssIndex = 0 #comment this out when you are ready to test with multiple IP's on the list
+    nextSS=ssInfo.pop(ssIndex)
+    # print(ssInfo)
+    #print(ssIndex)
+    #print(nextSS)
+
+    
+
     # local host IP '127.0.0.1'
-    host = '127.0.0.1'
+    host = nextSS[0] 
+    # host = '127.0.0.1' #Local Host overwrite
   
     # Define the port on which you want to connect
-    port = 2000
+    port = nextSS[1]
+    # port = 2000 #Local Host Overwrite
   
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-  
+    
     # connect to server on local computer
     s.connect((host,port))
   
     # message you send to server
-    message = input('input a message')
+    message=url
+    for x in ssInfo:
+
+        message=message+"\n"+str(x[0])+" "+str(x[1])
+    print("message being sent: \n", message)
+    
     while True:
   
         # message sent to server
         s.send(message.encode('ascii'))
-  
+        
         # messaga received from server
-        data = s.recv(1024)
-  
+        data = s.recv(262144)
+        data = data.decode("ascii")
         # print the received message
-        # here it would be a reverse of sent message
-        print('Received from the server :',str(data.decode('ascii')))
-  
+        concatdata = ''.join(data)
+        print('Received from the server :', concatdata)
+        #print(len(concatdata))
+
+        #HERE IS WHERE YOU WILL SAVE THE DATA FROM THE WGET REQUEST
+        
+        
+        #print(data)
+        
         # ask the client whether he wants to continue
         ans = input('\nDo you want to continue(y/n) :')
         if ans == 'y':
@@ -38,4 +93,4 @@ def Main():
     s.close()
   
 if __name__ == '__main__':
-    Main()
+    Main(sys.argv[1:])
