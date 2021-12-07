11d61b519576c143ac6480bceacb4c1a7d7cb448
alanalqobaisi <92046667+alanalqobaisi@users.noreply.github.com>
2021-12-07 12:20:09 -0700

    Update ss.py

--- ss.py
+++ ss.py
@@ -5,9 +5,9 @@
 # Member1 SIS ID: 831823119
 # Member1 Login ID: cbrock22
 
-# Member2 Name: XXXXXX
-# Member2 SIS ID: XXXXXX
-# Member2 Login ID: XXXXXX
+# Member2 Name: alanood alqobaisi
+# Member2 SIS ID: 832901420
+# Member2 Login ID: alanood
 
 # Member3 Name: Zach Walsh
 # Member3 SIS ID: 831955300
