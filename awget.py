5a6734d4d69831a4d019cd334091a320f85fa739
DaTimeKeeper <zachwalsh2219@live.com>
2021-12-07 21:59:30 -0700

    Names

--- awget.py
+++ awget.py
@@ -1,3 +1,18 @@
+###############################################
+# Group Name  : Uber
+
+# Member1 Name: Cole Brock
+# Member1 SIS ID: 831823119
+# Member1 Login ID: cbrock22
+
+# Member2 Name: Alanood Alqobaisi
+# Member2 SIS ID: 832901420
+# Member2 Login ID: alanood
+
+# Member3 Name: Zach Walsh
+# Member3 SIS ID: 831955300
+# Member3 Login ID: zachwals
+###############################################
 import socket
 import sys
 import random
